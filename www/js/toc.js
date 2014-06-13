dd7c355b971efc914d1754a1a4f8c4546fafc034
Christelle <christelle@ex-algebra.com>
2014-06-13 11:09:56 +0200

    controle de saisie
    controle saisie select et checkbox
    bouton suivant plus gros
    ajout page résultat

--- www/js/toc.js
+++ www/js/toc.js
@@ -1,1 +1,26 @@
+function valide_select(nom_select,action_vrai,action_faux){
 
+	valeur = document.multi_form[nom_select].value;
+
+	if (valeur != ''){
+		document.multi_form.action = action_vrai;
+	}else{
+		document.multi_form.action = action_faux;
+	}
+
+
+}
+function valide_slider(action_vrai,action_faux){
+	
+	if (document.multi_form.slidervalue.value == 50){
+		var r = confirm("Confirmez-vous la valeur du slider ?");
+		if (r == true) {
+			document.multi_form.action = action_vrai;
+		 }else{
+			 document.multi_form.action = action_faux;
+		 }
+	}else{
+		document.multi_form.action = action_vrai;
+	}
+	
+}
