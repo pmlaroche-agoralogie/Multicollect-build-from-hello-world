7ce8de82a9c293717d0af09ce9b2dffe9d2e5588
vmoore-paris <virginie.moore+github@gmail.com>
2014-07-21 17:05:17 +0200

    test notif

--- www/js/index.js
+++ www/js/index.js
@@ -268,10 +268,10 @@ function saveSession(firstTime) {
 	        		if (parseInt(surveys_config.test,10)) 
 	        		{
 	        			var test=1;
-	        			/*duration = 60*3; //dure 3 min
-                    	var ecarttest = (60*5); //toutes les 5 min*/
-	        			duration = 30; //dure 3 min
-                    	var ecarttest = 60; //toutes les 5 min
+	        			duration = 60*3; //dure 3 min
+                    	var ecarttest = (60*5); //toutes les 5 min
+	        			/*duration = 30; //dure 3 min
+                    	var ecarttest = 60; //toutes les 5 min*/
 	        		}
 	        		if ((firstTime) && (test!=0))
 	        		{	//première ligne pour test dans 5 min si pas mode test
@@ -290,7 +290,7 @@ function saveSession(firstTime) {
 	        			            		
 	        			            		_timestampSessionNotif = new Date(timestampSession*1000);
 	        			            		lastID = parseInt(results.insertId,10);
-	        			            		alert(lastID);
+	        			            		//alert(lastID);
 	        	        				    window.plugin.notification.local.add({
 	        	        				                                         id:      lastID,
 	        	        				                                         title:   'Application de Suivi',
